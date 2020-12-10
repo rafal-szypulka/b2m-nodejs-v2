f51be7735f7759b9de2a5c4c7400790bfeb313a6
nickfreer <nick.freer@yahoo.co.uk>
2020-12-10 14:03:27 +0100

    NDF - Fixed json comments problem, added description for creating ocp add from Dockerfile with new app directory from lab4

--- lab-4/app/server.js
+++ lab-4/app/server.js
@@ -1,6 +1,7 @@
 'use strict'
 const { createLogger, format, transports } = require('winston')
 const express = require('express')
+const Prometheus = require('prom-client')
 
 const logger = createLogger({
   level: 'debug',
@@ -16,8 +17,28 @@ const logger = createLogger({
 var health = true;
 var msg;
 
+const metricsInterval = Prometheus.collectDefaultMetrics()
+
 const app = express()
-const port = process.env.PORT || 3002
+const port = process.env.PORT || 3003
+
+const checkoutsTotal = new Prometheus.Counter({
+  name: 'checkouts_total',
+  help: 'Total number of checkouts',
+  labelNames: ['payment_method']
+})
+
+const httpRequestDurationMicroseconds = new Prometheus.Histogram({
+  name: 'http_request_duration_ms',
+  help: 'Duration of HTTP requests in ms',
+  labelNames: ['method', 'route', 'code'],
+  buckets: [0.10, 5, 15, 50, 100, 200, 300, 400, 500]  // buckets for response time from 0.1ms to 500ms
+})
+
+app.use((req, res, next) => {
+  res.locals.startEpoch = Date.now()
+  next()
+})
 
 app.get('/', (req, res, next) => {
   res.redirect('/checkout')
@@ -41,7 +62,9 @@ app.get('/bad-health', (req, res, next) => {
 app.get('/checkout', (req, res, next) => {
   const paymentMethod = Math.round(Math.random() * 100) > 20 ? 'card' : 'paypal'
   const errorState =  Math.round(Math.random() * 100) > 20 ? 0 : 1
-
+  checkoutsTotal.inc({
+    payment_method: paymentMethod
+  })
   var delay = Math.round(Math.random() * 100);
   if (errorState) {
     msg = 'RSAP0010E: Severe problem detected'
@@ -57,10 +80,23 @@ app.get('/checkout', (req, res, next) => {
   }
 })
 
+app.get('/metrics', (req, res) => {
+  res.set('Content-Type', Prometheus.register.contentType)
+  res.end(Prometheus.register.metrics())
+})
 
 app.use((err, req, res, next) => {
   res.statusCode = 500
   res.json({ error: err.message })
+  next()
+})
+
+app.use((req, res, next) => {
+  const responseTimeInMs = Date.now() - res.locals.startEpoch
+
+  httpRequestDurationMicroseconds
+    .labels(req.method, req.route.path, res.statusCode)
+    .observe(responseTimeInMs)
   next()
 })
 
@@ -69,12 +105,14 @@ const server = app.listen(port, () => {
 })
 
 process.on('SIGTERM', () => {
+  clearInterval(metricsInterval)
 
   server.close((err) => {
     if (err) {
       console.error(err)
       process.exit(1)
     }
+
     process.exit(0)
   })
 })
